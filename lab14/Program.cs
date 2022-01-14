ad66df5f490652ebe844073ecdca37e1b409cefd
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2022-01-14 22:50:44 +1000

    Добавьте файлы проекта.

---
+++ lab14/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*Разработать классы Cat и Dog, которые реализуют абстрактный класс Animal. В классах реализовать следующие элементы:
+•	свойство – название животного;
+•	метод Say(), выводящий на экран «Мяу» либо «Гав» соответственно.
+Создайте экземпляры классов Cat и Dog и проверьте их работоспособность.*/
+
+
+
+namespace lab14
+{
+
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+            }
+        public abstract void Say();
+        public  void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+
+    }
+    class Cat : Animal
+    {
+       // private string taste;
+        public string name;
+        public override string Name
+        { get => name; 
+            set => name=value;
+        }
+        public Cat(string name/*string taste*/)
+            :base (name)
+        {
+           // this.taste = taste;
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу!");
+        }
+    }
+    class Dog : Animal
+    {
+        
+        public string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Dog(string name)
+            : base(name)
+        {
+            
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+            
+
+            //создание стека
+
+            var stack = new Stack<string>();
+            stack.Push("cat");
+            stack.Push("dog");
+            
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(stack.Pop());
+
+            }
+            Console.ReadKey();
+        }
+    }
+   
+}
+
+
---
+++ probniivariant/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probniivariant
+{
+  
+    class All
+    {
+        virtual public void Say()
+        { Console.WriteLine("Hi"); }
+    }
+    class Animal : All
+    { }
+    
+
+    class Cat : Animal
+    {
+        override public void Say()
+        { 
+            Console.WriteLine("мяу");
+        }
+    }
+    class Dog : Animal
+    {
+        override public void Say()
+        { Console.WriteLine("гав"); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+          
+
+            //создание стека
+
+            var stack = new Stack<string>();
+            stack.Push("cat");
+            stack.Push("dog");
+          
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(stack.Pop());
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+
